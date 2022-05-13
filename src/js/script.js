349c7668fe00e0fdb93baf5cb395591cccddd6be
VVasilevich <vve913@gmail.com>
2022-05-13 23:06:21 +0600

    add responsiveness slider catalog feed

--- src/js/script.js
+++ src/js/script.js
@@ -3,25 +3,41 @@ $(document).ready(function(){
 		speed: 1200,
 		adaptiveHeight: true,
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left.svg"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right.svg"></button>'
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right.svg"></button>',
+		responsive: [
+			{
+			  breakpoint: 767,
+			  settings: {
+				arrows: false,
+				dots: true
+			  }
+			},
+			{
+			  breakpoint: 575,
+			  settings: {
+				arrows: false,
+				dots: true
+			  }
+			}
+		]
 	});
 
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-        $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-    });
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+		$(this)
+		  .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+		  .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
 
 	function toggleSlide(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
-                e.preventDefault();
-                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__more').eq(i).toggleClass('catalog-item__more_active');
-            })
-        });
-    };
+		$(item).each(function(i) {
+			$(this).on('click', function(e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__more').eq(i).toggleClass('catalog-item__more_active');
+			})
+		});
+	};
 
-    toggleSlide('.catalog-item__link');
-    toggleSlide('.catalog-item__back');
+	toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__back');
 });
