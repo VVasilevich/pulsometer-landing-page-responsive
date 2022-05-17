ffc638fc43e02b58e790a9db64120a974a43532e
VVasilevich <vve913@gmail.com>
2022-05-17 23:31:08 +0600

    add smart.php

--- src/js/script.js
+++ src/js/script.js
@@ -92,4 +92,22 @@ $(document).ready(function(){
     validateForms('#order form');
 
 	$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+		if (!$(this).valid()) {
+			return;
+		}
+		$.ajax({
+			type: "POST",
+			url: "php/mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+			$('form').trigger('reset');
+		});
+		return false;
+	});
 });
